d907c77b5b09a762ecb9054844228b7ab98a63c9
nsarkar <nsarkar@login007.inband>
2025-11-01 15:28:19 -0400

    minor changes

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,8 @@
 sbatch <<EOT
 #!/bin/bash
-#SBATCH --partition=      # Partition name
+#SBATCH --partition=       # Partition name
 #SBATCH --gres=gpu:h200:2              # Number of GPUs
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=300G
 
 export NCCL_P2P_DISABLE=1
@@ -11,6 +11,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_HOME=$(dirname $(dirname $(which nvcc)))
 export WANDB_PROJECT=GPTeacher
 
-cd LLaMA-Factory
+cd ../LLaMA-Factory
+echo "Training Qwen3.0-6B Filter..."
 llamafactory-cli train ../configs/qwen3_0-6B_filter.yaml
 EOT
